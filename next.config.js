7f3b75be908e90834e06241cfd74227c01bca29a
Emdadul <emdadulislam580@gmail.com>
2024-01-03 23:19:29 +0600

    style: globals color changed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
-const withMDX = require('@next/mdx')()
- 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-}
- 
-module.exports = withMDX(nextConfig)
+const nextConfig = {}
+
+module.exports = nextConfig
